1c1bbdf868547535b9770552ce83187508975d6e
Joaquin Ogallar <joaquinnogallar@gmail.com>
2026-01-31 12:21:25 -0300

    feat: add basic navigation between menus in game

--- Assets/_Scripts/Input/MenuManager.cs
+++ Assets/_Scripts/Input/MenuManager.cs
@@ -1,29 +1,39 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class MenuManager : MonoBehaviour
 {
     [Header("Menus")]
     [SerializeField] private GameObject pauseMenu;
     [SerializeField] private GameObject optionsMenu;
-    [SerializeField] private GameObject audioMenu;
+    [SerializeField] private GameObject audioVideoMenu;
+    [SerializeField] private GameObject confirmExitMenu;
 
     private GameObject currentMenu;
+    private Stack<GameObject> menuStack = new Stack<GameObject>();
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (currentMenu == null)
+            {
                 OpenMenu(pauseMenu);
+            }
             else
-                CloseCurrentMenu();
+            {
+                Back();
+            }
         }
     }
 
     public void OpenMenu(GameObject menu)
     {
         if (currentMenu != null)
+        {
+            menuStack.Push(currentMenu);
             currentMenu.SetActive(false);
+        }
 
         currentMenu = menu;
         currentMenu.SetActive(true);
@@ -32,23 +42,26 @@ public class MenuManager : MonoBehaviour
         Time.timeScale = 0f;
     }
 
-    public void CloseCurrentMenu()
+    public void Back()
     {
-        if (currentMenu == null) return;
+        if (currentMenu != null)
+            currentMenu.SetActive(false);
 
-        currentMenu.SetActive(false);
-        currentMenu = null;
-
-        Time.timeScale = 1f;
+        if (menuStack.Count > 0)
+        {
+            currentMenu = menuStack.Pop();
+            currentMenu.SetActive(true);
+            currentMenu.transform.SetAsLastSibling();
+        }
+        else
+        {
+            currentMenu = null;
+            Time.timeScale = 1f;
+        }
     }
 
-    public void OpenOptions()
-    {
-        OpenMenu(optionsMenu);
-    }
-
-    public void OpenAudio()
-    {
-        OpenMenu(audioMenu);
-    }
+    // Métodos para botones
+    public void OpenOptions() => OpenMenu(optionsMenu);
+    public void OpenAudioVideo() => OpenMenu(audioVideoMenu);
+    public void OpenConfirmExit() => OpenMenu(confirmExitMenu);
 }
